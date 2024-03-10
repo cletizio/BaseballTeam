073532ed5c068fc6dd9b9fd528a349a03da05ff8
Chris Letizio <cletizio@gmail.com>
2024-03-10 13:54:11 -0400

    Initial
    Initial Commit with only main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    // Static scanner will be used for other classes to take input
+    static Scanner input = new Scanner (System.in);
+    public static void main(String[] args) {
+        public static void main (String [] args){
+
+            // Creates a new Personnel List
+            Roster baseballPlayers = new Roster ();
+
+            // Print player records
+            baseballPlayers.printRecord();
+
+            // Update player records
+            baseballPlayers.teamUpdate();
+
+            // Print updated player records
+            baseballPlayers.printRecord();
+    }
+}
