51a7bc0fcf0c3e3cb894660036c44eb46fff9094
Chris Letizio <cletizio@gmail.com>
2024-03-11 19:19:00 -0400

    Try/Catch added to Roster
    Added a try/catch statement to Roster class.

--- src/Roster.java
+++ src/Roster.java
@@ -5,58 +5,62 @@ public class Roster {
     int numPlayers;
 
     Roster() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the number of players on the team: ");
-        numPlayers = scanner.nextInt();
-        scanner.nextLine(); // Consumes new line leftover
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter the number of players on the team: ");
+            numPlayers = scanner.nextInt();
+            scanner.nextLine(); // Consumes new line leftover
 
-        teamRoster = new Player[numPlayers];  // Creates a reference array of 7 players
+            teamRoster = new Player[numPlayers];  // Creates a reference array of 7 players
 
-        // Each team member is added to the array, though each one is a Player, they
-        // each are created using their appropriate subclass
-        for (int i = 0; i < numPlayers; i++) {
-            System.out.println("Enter player type (1 for Pitcher or 2 for Fielder)");
-            int playerType = scanner.nextInt();
-            scanner.nextLine();
+            // Each team member is added to the array, though each one is a Player, they
+            // each are created using their appropriate subclass
+            for (int i = 0; i < numPlayers; i++) {
+                System.out.println("Enter player type (1 for Pitcher or 2 for Fielder)");
+                int playerType = scanner.nextInt();
+                scanner.nextLine();
 
-            System.out.println("Enter player first name: ");
-            String firstName = scanner.nextLine();
+                System.out.println("Enter player first name: ");
+                String firstName = scanner.nextLine();
 
-            System.out.println("Enter player last name: ");
-            String lastName = scanner.nextLine();
+                System.out.println("Enter player last name: ");
+                String lastName = scanner.nextLine();
 
-            System.out.println("Enter player address: ");
-            String address = scanner.nextLine();
+                System.out.println("Enter player address: ");
+                String address = scanner.nextLine();
 
-            System.out.println("Enter player phone number: ");
-            String phone = scanner.nextLine();
+                System.out.println("Enter player phone number: ");
+                String phone = scanner.nextLine();
 
-            System.out.println("Enter player batting hand (Left/Right): ");
-            char bats = scanner.nextLine().charAt(0);
+                System.out.println("Enter player batting hand (Left/Right): ");
+                char bats = scanner.nextLine().charAt(0);
 
-            System.out.println("Enter player throwing hand (Left/Right): ");
-            char throwHand = scanner.nextLine().charAt(0);
+                System.out.println("Enter player throwing hand (Left/Right): ");
+                char throwHand = scanner.nextLine().charAt(0);
 
 
-            System.out.println("Enter player number: ");
-            int number = scanner.nextInt();
-            scanner.nextLine();
+                System.out.println("Enter player number: ");
+                int number = scanner.nextInt();
+                scanner.nextLine();
 
-            if (playerType == 1) {
-                System.out.println("Enter pitcher type Starter/Reliever (S/R): ");
-                char pitchType = scanner.nextLine().charAt(0);
-                teamRoster[i] = new Pitcher(firstName, lastName, address, phone, bats,
-                        throwHand, number, pitchType);
-            } else if (playerType == 2) {
-                System.out.println("Enter player position: ");
-                String position = scanner.nextLine();
-                teamRoster[i] = new FieldPosition(firstName, lastName, address, phone, bats, throwHand, number, position);
-            } else {
-                System.out.println("Invalid player type:");
-                i--; //Decrement i to retry the loop.
+                if (playerType == 1) {
+                    System.out.println("Enter pitcher type Starter/Reliever (S/R): ");
+                    char pitchType = scanner.nextLine().charAt(0);
+                    teamRoster[i] = new Pitcher(firstName, lastName, address, phone, bats,
+                            throwHand, number, pitchType);
+                } else if (playerType == 2) {
+                    System.out.println("Enter player position: ");
+                    String position = scanner.nextLine();
+                    teamRoster[i] = new FieldPosition(firstName, lastName, address, phone, bats, throwHand, number, position);
+                } else {
+                    System.out.println("Invalid player type:");
+                    i--; //Decrement i to retry the loop.
+                }
             }
+            scanner.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        scanner.close();
     }
 
     // teamUpdate method, updates each team member with the update method from their
